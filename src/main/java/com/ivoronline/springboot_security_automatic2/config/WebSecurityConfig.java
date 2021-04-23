ba475046fd8d78c9f735d6501fb82bc6f2908d07
igoreta <ivoronline@gmail.com>
2021-04-23 11:40:12 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_security_automatic2/config/WebSecurityConfig.java
+++ src/main/java/com/ivoronline/springboot_security_automatic2/config/WebSecurityConfig.java
@@ -1,16 +1,12 @@
-package com.ivoronline.springboot_security_automatic2.security;
+package com.ivoronline.springboot_security_automatic2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -21,11 +17,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   //=================================================================
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-    //CREATE USERS
     auth.inMemoryAuthentication().withUser("myadmin").password("myadminpassword").roles("ADMIN");
     auth.inMemoryAuthentication().withUser("myuser" ).password("myuserpassword1" ).roles("USER" );
-
   }
 
   //=======================================================================
